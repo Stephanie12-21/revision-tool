9c1d2ae48a86cbaef9eae32de3cd6b8a304f1349
Stéphanie Maminiaina <stephaniepageot42@gmail.com>
2025-09-14 15:53:48 +0300

    add PWA functionality with build successfull

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,15 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-//   allowedDevOrigins: [
-//     "http://localhost:3000", // ton frontend local
-//     "https://revision-tool-backend.onrender.com", // ton backend déployé
-//   ],
-// };
-
-// export default nextConfig;
-import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: [
-    "http://localhost:3000", // ton frontend local
-    "https://revision-tool-backend.onrender.com", // ton backend déployé
+    "http://localhost:3000",
+    "https://revision-tool-backend.onrender.com",
   ],
 };
 
 export default withPWA({
-  dest: "public", // dossier où seront générés les fichiers du service worker
-  register: true, // enregistre automatiquement le SW
-  skipWaiting: true, // active le nouveau SW sans attendre
+  dest: "public",
+  register: true,
+  skipWaiting: true,
 })(nextConfig);
